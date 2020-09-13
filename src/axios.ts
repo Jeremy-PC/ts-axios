17a9ce20e4e7fcfc780e2e622a626b2aff5fdd45
jeremy <jeremypc@163.com>
2020-09-13 23:52:38 +0800

    feat(src): 增加config配置

--- src/axios.ts
+++ src/axios.ts
@@ -1,13 +1,14 @@
-import { AxiosInstance } from './types'
+import { AxiosInstance, AxiosRequestConfig } from './types'
 import Axios from './core/Axios';
 import { extend } from './helpers/util'
+import defaults from './defaults';
 
-function creatInstance(): AxiosInstance {
-    const context = new Axios()
+function creatInstance(config: AxiosRequestConfig): AxiosInstance {
+    const context = new Axios(config)
     const instance = Axios.prototype.request.bind(context)
     extend(instance, context)
     return instance as AxiosInstance
 }
-const axios = creatInstance()
+const axios = creatInstance(defaults)
 
 export default axios
